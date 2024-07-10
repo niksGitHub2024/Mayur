77e9788665a47ca0b2d90cbeaab7879c70ff7dea
niksGitHub2024 <nikhildeshmukh8484@gmail.com>
2024-07-10 14:28:26 +0530

    Add m1.java
    Self

--- PracticePro/src/com/may/Test2.java
+++ PracticePro/src/com/may/Test2.java
@@ -2,6 +2,11 @@ package com.may;
 
 public class Test2 {
 
+	public void m1()
+	{
+	System.out.println("It's me Mayur");
+	
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("This is mayur's project");
